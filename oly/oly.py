216a1966f41ca529d77b25490f535ed54c44c633
Rachel Rios <rachelrios@oteotsxg6ryjhml.tfayd.com>
2021-04-18 19:56:11 -0400

    Sample Graphics and CSV file from Kaggle

---
+++ oly/oly.py
@@ -0,0 +1,10 @@
+### Define Libraries and other items ###
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree
+import matplotlib.pyplot as plt
